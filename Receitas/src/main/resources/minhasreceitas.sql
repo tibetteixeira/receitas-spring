03e72ba0484545604025365395069119d43388ff
tibetteixeira <tibetbrasil@gmail.com>
2022-10-07 02:54:01 -0300

    Ajuste no sql

--- Receitas/src/main/resources/minhasreceitas.sql
+++ Receitas/src/main/resources/minhasreceitas.sql
@@ -3,29 +3,29 @@ DROP DATABASE IF EXISTS minhasreceitas;
 CREATE DATABASE minhasreceitas;
 
 CREATE TABLE categoria (
-    id INTEGER,
-    descricao VARCHAR(50),
+    id INTEGER NOT NULL,
+    descricao VARCHAR(50) NOT NULL,
     CONSTRAINT pk_categoria PRIMARY KEY (id)
 );
 
 CREATE TABLE unidade (
-    sigla VARCHAR(3),
-    descricao VARCHAR(20),
+    sigla VARCHAR(3) NOT NULL,
+    descricao VARCHAR(20) NOT NULL,
     CONSTRAINT pk_unidade PRIMARY KEY (sigla)
 );
 
 CREATE TABLE produto (
-    id INTEGER,
-    descricao VARCHAR(100),
+    id INTEGER NOT NULL,
+    descricao VARCHAR(100) NOT NULL,
     CONSTRAINT pk_produto PRIMARY KEY (id)
 );
 
 CREATE TABLE usuario (
-    id INTEGER,
-    senha VARCHAR(255),
-    email VARCHAR(100),
-    nome VARCHAR(20),
-    sobrenome VARCHAR(20),
+    id INTEGER NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    nome VARCHAR(20) NOT NULL,
+    sobrenome VARCHAR(20) NOT NULL,
     data_criacao TIMESTAMP,
     data_atualizacao TIMESTAMP,
     data_exclusao TIMESTAMP,
@@ -33,15 +33,15 @@ CREATE TABLE usuario (
 );
 
 CREATE TABLE receita (
-    id INTEGER,
-    titulo VARCHAR(100),
-    descricao VARCHAR(255),
-    tempo_preparo INTEGER,
-    porcao INTEGER,
+    id INTEGER NOT NULL,
+    titulo VARCHAR(100) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    tempo_preparo INTEGER NOT NULL,
+    porcao INTEGER NOT NULL,
     data_criacao TIMESTAMP,
     data_atualizacao TIMESTAMP,
     data_exclusao TIMESTAMP,
-    id_usuario INTEGER,
+    id_usuario INTEGER NOT NULL,
     id_categoria INTEGER,
     CONSTRAINT pk_receita PRIMARY KEY (id),
     CONSTRAINT fk_receita_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id),
@@ -49,30 +49,37 @@ CREATE TABLE receita (
 );
 
 CREATE TABLE ingrediente (
-    id INTEGER,
-    quantidade NUMERIC(11,2),
-    sigla_unidade VARCHAR(3),
-    id_produto INTEGER,
+    id INTEGER NOT NULL,
+    quantidade NUMERIC(11,2) NOT NULL,
+    sigla_unidade VARCHAR(3) NOT NULL,
+    id_produto INTEGER NOT NULL,
     CONSTRAINT pk_ingrediente PRIMARY KEY (id),
     CONSTRAINT fk_ingrediente_produto FOREIGN KEY (id_produto) REFERENCES produto(id),
     CONSTRAINT fk_ingrediente_unidade FOREIGN KEY (sigla_unidade) REFERENCES unidade(sigla)
 );
 
+CREATE TABLE ingrediente_receita (
+    id_ingrediente INTEGER NOT NULL,
+    id_receita INTEGER NOT NULL,
+    CONSTRAINT fk_ingrediente_receita_ingrediente FOREIGN KEY (id_ingrediente) REFERENCES ingrediente(id),
+    CONSTRAINT fk_ingrediente_receita_receita FOREIGN KEY (id_receita) REFERENCES receita(id)
+);
+
 CREATE TABLE avaliacao_receita (
-    id_receita INTEGER,
-    id_usuario INTEGER,
-    score INTEGER,
+    id_receita INTEGER NOT NULL,
+    id_usuario INTEGER NOT NULL,
+    score INTEGER NOT NULL,
     CONSTRAINT pk_avaliacao_receita PRIMARY KEY (id_usuario, id_receita),
     CONSTRAINT fk_avaliacao_receita_receita FOREIGN KEY (id_receita) REFERENCES receita(id),
     CONSTRAINT fk_avaliacao_receita_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id)
 );
 
 CREATE TABLE comentario (
-    id INTEGER,
-    titulo VARCHAR(100),
-    comentario VARCHAR(255),
-    id_receita INTEGER,
-    id_usuario INTEGER,
+    id INTEGER NOT NULL,
+    titulo VARCHAR(100) NOT NULL,
+    comentario VARCHAR(255) NOT NULL,
+    id_receita INTEGER NOT NULL,
+    id_usuario INTEGER NOT NULL,
     data_inclucao TIMESTAMP,
     data_exclusao TIMESTAMP,
     CONSTRAINT pk_comentario PRIMARY KEY (id),
@@ -81,17 +88,16 @@ CREATE TABLE comentario (
 );
 
 CREATE TABLE modo_preparo (
-    ordem INTEGER,
-    id_receita INTEGER,
-    descricao VARCHAR(255),
+    ordem INTEGER NOT NULL,
+    id_receita INTEGER NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
     CONSTRAINT pk_modo_preparo PRIMARY KEY (id_receita, ordem),
     CONSTRAINT fk_modo_preparo_receita FOREIGN KEY (id_receita) REFERENCES receita(id)
 );
 
 CREATE TABLE receitas_favoritas (
-    id_receita INTEGER,
-    id_usuario INTEGER,
-    CONSTRAINT pk_receitas_favoritas PRIMARY KEY (id_usuario, id_receita),
+    id_receita INTEGER NOT NULL,
+    id_usuario INTEGER NOT NULL,
     CONSTRAINT fk_receitas_favoritas_receita FOREIGN KEY (id_receita) REFERENCES receita(id),
     CONSTRAINT fk_receitas_favoritas_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id)
 );
