90d7ecbeb8c46fa03c9c30de5e2a5a65b9ab34b7
tibetteixeira <tibetbrasil@gmail.com>
2022-10-14 02:47:51 -0300

    It updates script sql

--- Receitas/src/main/resources/minhasreceitas.sql
+++ Receitas/src/main/resources/minhasreceitas.sql
@@ -1,103 +1,110 @@
-DROP DATABASE IF EXISTS minhasreceitas;
+-- DROP DATABASE IF EXISTS minhasrecipes;
 
-CREATE DATABASE minhasreceitas;
+-- CREATE DATABASE minhasrecipes;
 
-CREATE TABLE categoria (
-    id INTEGER NOT NULL,
-    descricao VARCHAR(50) NOT NULL,
-    CONSTRAINT pk_categoria PRIMARY KEY (id)
+CREATE SEQUENCE tb_category_id_seq START 1 INCREMENT 1;
+CREATE TABLE tb_category (
+    category_id INTEGER NOT NULL DEFAULT nextval('tb_category_id_seq'),
+    description VARCHAR(50) NOT NULL,
+    CONSTRAINT pk_category PRIMARY KEY (category_id)
 );
 
-CREATE TABLE unidade (
-    sigla VARCHAR(3) NOT NULL,
-    descricao VARCHAR(20) NOT NULL,
-    CONSTRAINT pk_unidade PRIMARY KEY (sigla)
+CREATE TABLE tb_unit (
+    abbr VARCHAR(3) NOT NULL,
+    description VARCHAR(20) NOT NULL,
+    CONSTRAINT pk_unit PRIMARY KEY (abbr)
 );
 
-CREATE TABLE produto (
-    id INTEGER NOT NULL,
-    descricao VARCHAR(100) NOT NULL,
-    CONSTRAINT pk_produto PRIMARY KEY (id)
+CREATE SEQUENCE tb_product_id_seq START 1 INCREMENT 1;
+CREATE TABLE tb_product (
+    product_id INTEGER NOT NULL DEFAULT nextval('tb_product_id_seq'),
+    description VARCHAR(100) NOT NULL,
+    CONSTRAINT pk_product PRIMARY KEY (product_id)
 );
 
-CREATE TABLE usuario (
-    id INTEGER NOT NULL,
-    senha VARCHAR(255) NOT NULL,
+CREATE SEQUENCE tb_user_id_seq START 1 INCREMENT 1;
+CREATE TABLE tb_user (
+    user_id INTEGER NOT NULL DEFAULT nextval('tb_user_id_seq'),
+    password VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    nome VARCHAR(20) NOT NULL,
-    sobrenome VARCHAR(20) NOT NULL,
-    data_criacao TIMESTAMP,
-    data_atualizacao TIMESTAMP,
-    data_exclusao TIMESTAMP,
-    CONSTRAINT pk_usuario PRIMARY KEY (id)
+    name VARCHAR(20) NOT NULL,
+    lastname VARCHAR(20) NOT NULL,
+    creation_date TIMESTAMP,
+    update_date TIMESTAMP,
+    exclusion_date TIMESTAMP,
+    CONSTRAINT pk_user PRIMARY KEY (user_id)
 );
 
-CREATE TABLE receita (
-    id INTEGER NOT NULL,
-    titulo VARCHAR(100) NOT NULL,
-    descricao VARCHAR(255) NOT NULL,
-    tempo_preparo INTEGER NOT NULL,
-    porcao INTEGER NOT NULL,
-    data_criacao TIMESTAMP,
-    data_atualizacao TIMESTAMP,
-    data_exclusao TIMESTAMP,
-    id_usuario INTEGER NOT NULL,
-    id_categoria INTEGER,
-    CONSTRAINT pk_receita PRIMARY KEY (id),
-    CONSTRAINT fk_receita_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id),
-    CONSTRAINT fk_receita_categoria FOREIGN KEY (id_categoria) REFERENCES categoria(id)
+
+CREATE SEQUENCE tb_recipe_id_seq START 1 INCREMENT 1;
+CREATE TABLE tb_recipe (
+    recipe_id INTEGER NOT NULL DEFAULT nextval('tb_recipe_id_seq'),
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    preparationTime INTEGER NOT NULL,
+    portion INTEGER NOT NULL,
+    creation_date TIMESTAMP,
+    update_date TIMESTAMP,
+    exclusion_date TIMESTAMP,
+    user_id INTEGER NOT NULL,
+    category_id INTEGER,
+    CONSTRAINT pk_recipe PRIMARY KEY (recipe_id),
+    CONSTRAINT fk_recipe_user FOREIGN KEY (user_id) REFERENCES tb_user(user_id),
+    CONSTRAINT fk_recipe_category FOREIGN KEY (category_id) REFERENCES tb_category(category_id)
 );
 
-CREATE TABLE ingrediente (
-    id INTEGER NOT NULL,
-    quantidade NUMERIC(11,2) NOT NULL,
-    sigla_unidade VARCHAR(3) NOT NULL,
-    id_produto INTEGER NOT NULL,
-    CONSTRAINT pk_ingrediente PRIMARY KEY (id),
-    CONSTRAINT fk_ingrediente_produto FOREIGN KEY (id_produto) REFERENCES produto(id),
-    CONSTRAINT fk_ingrediente_unidade FOREIGN KEY (sigla_unidade) REFERENCES unidade(sigla)
+CREATE SEQUENCE tb_ingredient_id_seq START 1 INCREMENT 1;
+CREATE TABLE tb_ingredient (
+    ingredient_id INTEGER NOT NULL DEFAULT nextval('tb_ingredient_id_seq'),
+    amount NUMERIC(11,2) NOT NULL,
+    abbr_unit VARCHAR(3) NOT NULL,
+    product_id INTEGER NOT NULL,
+    CONSTRAINT pk_ingredient PRIMARY KEY (ingredient_id),
+    CONSTRAINT fk_ingredient_product FOREIGN KEY (product_id) REFERENCES tb_product(product_id),
+    CONSTRAINT fk_ingredient_unit FOREIGN KEY (abbr_unit) REFERENCES tb_unit(abbr)
 );
 
-CREATE TABLE ingrediente_receita (
-    id_ingrediente INTEGER NOT NULL,
-    id_receita INTEGER NOT NULL,
-    CONSTRAINT fk_ingrediente_receita_ingrediente FOREIGN KEY (id_ingrediente) REFERENCES ingrediente(id),
-    CONSTRAINT fk_ingrediente_receita_receita FOREIGN KEY (id_receita) REFERENCES receita(id)
+CREATE TABLE tb_ingredient_recipe (
+    ingredient_id INTEGER NOT NULL,
+    recipe_id INTEGER NOT NULL,
+    CONSTRAINT fk_ingredient_recipe_ingredient FOREIGN KEY (ingredient_id) REFERENCES tb_ingredient(ingredient_id),
+    CONSTRAINT fk_ingredient_recipe_recipe FOREIGN KEY (recipe_id) REFERENCES tb_recipe(recipe_id)
 );
 
-CREATE TABLE avaliacao_receita (
-    id_receita INTEGER NOT NULL,
-    id_usuario INTEGER NOT NULL,
+CREATE TABLE tb_rating (
+    recipe_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
     score INTEGER NOT NULL,
-    CONSTRAINT pk_avaliacao_receita PRIMARY KEY (id_usuario, id_receita),
-    CONSTRAINT fk_avaliacao_receita_receita FOREIGN KEY (id_receita) REFERENCES receita(id),
-    CONSTRAINT fk_avaliacao_receita_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id)
+    CONSTRAINT pk_rating PRIMARY KEY (user_id, recipe_id),
+    CONSTRAINT fk_rating_recipe FOREIGN KEY (recipe_id) REFERENCES tb_recipe(recipe_id),
+    CONSTRAINT fk_rating_user FOREIGN KEY (user_id) REFERENCES tb_user(user_id)
 );
 
-CREATE TABLE comentario (
-    id INTEGER NOT NULL,
-    titulo VARCHAR(100) NOT NULL,
-    comentario VARCHAR(255) NOT NULL,
-    id_receita INTEGER NOT NULL,
-    id_usuario INTEGER NOT NULL,
-    data_inclucao TIMESTAMP,
-    data_exclusao TIMESTAMP,
-    CONSTRAINT pk_comentario PRIMARY KEY (id),
-    CONSTRAINT fk_comentario_receita FOREIGN KEY (id_receita) REFERENCES receita(id),
-    CONSTRAINT fk_comentario_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id)
+CREATE SEQUENCE tb_comment_id_seq START 1 INCREMENT 1;
+CREATE TABLE tb_comment (
+    comment_id INTEGER NOT NULL DEFAULT nextval('tb_comment_id_seq'),
+    title VARCHAR(100) NOT NULL,
+    comment_desc VARCHAR(255) NOT NULL,
+    recipe_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    creation_date TIMESTAMP,
+    exclusion_date TIMESTAMP,
+    CONSTRAINT pk_comment PRIMARY KEY (comment_id),
+    CONSTRAINT fk_comment_recipe FOREIGN KEY (recipe_id) REFERENCES tb_recipe(recipe_id),
+    CONSTRAINT fk_comment_user FOREIGN KEY (user_id) REFERENCES tb_user(user_id)
 );
 
-CREATE TABLE modo_preparo (
-    ordem INTEGER NOT NULL,
-    id_receita INTEGER NOT NULL,
-    descricao VARCHAR(255) NOT NULL,
-    CONSTRAINT pk_modo_preparo PRIMARY KEY (id_receita, ordem),
-    CONSTRAINT fk_modo_preparo_receita FOREIGN KEY (id_receita) REFERENCES receita(id)
+CREATE TABLE tb_method_preparation (
+    "order" INTEGER NOT NULL,
+    recipe_id INTEGER NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    CONSTRAINT pk_method_preparation PRIMARY KEY (recipe_id, "order"),
+    CONSTRAINT fk_method_preparation_recipe FOREIGN KEY (recipe_id) REFERENCES tb_recipe(recipe_id)
 );
 
-CREATE TABLE receitas_favoritas (
-    id_receita INTEGER NOT NULL,
-    id_usuario INTEGER NOT NULL,
-    CONSTRAINT fk_receitas_favoritas_receita FOREIGN KEY (id_receita) REFERENCES receita(id),
-    CONSTRAINT fk_receitas_favoritas_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id)
+CREATE TABLE tb_favorite_recipes (
+    recipe_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    CONSTRAINT fk_favorite_recipes_recipe FOREIGN KEY (recipe_id) REFERENCES tb_recipe(recipe_id),
+    CONSTRAINT fk_favorite_recipes_user FOREIGN KEY (user_id) REFERENCES tb_user(user_id)
 );
