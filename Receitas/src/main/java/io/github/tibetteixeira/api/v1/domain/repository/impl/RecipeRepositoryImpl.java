6f724dc1f9541f1061f4a0f3cf3af86b0b76857b
tibetteixeira <tibetbrasil@gmail.com>
2022-10-24 23:25:02 -0300

    It deletes unused files

--- Receitas/src/main/java/io/github/tibetteixeira/api/v1/domain/repository/impl/RecipeRepositoryImpl.java
+++
@@ -1,8 +0,0 @@
-package io.github.tibetteixeira.api.v1.domain.repository.impl;
-
-import io.github.tibetteixeira.api.v1.domain.repository.RecipeRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class RecipeRepositoryImpl implements RecipeRepository {
-}
